490619ca7d2c0f039c55850d94f6744882237d40
Aline Dubois <juillet.aline@gmail.com>
2021-05-24 12:10:35 +0200

    ménage dans le code

--- src/components/series/Serie.js
+++ src/components/series/Serie.js
@@ -1,10 +1,10 @@
 import "./Serie.css";
 import {Link} from "react-router-dom";
 
-export const Serie = ({poster, title, id, suppressionSerie}) => {
+export const Serie = ({poster, title, id}) => {
     return (
         <Link to={`/serie/${id}`}>
-            <div className="serie" /*onClick={()=>suppressionSerie(id)}*/>
+            <div className="serie">
                 <img className="image-serie" src={poster} alt={title}/>
             </div>
         </Link>
